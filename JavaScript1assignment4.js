60fc5c1692645babd55650775003dd4c0dd06d7d
piyushchanna <piyush.ojha@newgen.co.in>
2016-06-19 21:42:05 -0700

    Update JavaScript1assignment4.js

--- JavaScript1assignment4.js
+++ JavaScript1assignment4.js
@@ -1,10 +1,17 @@
 array = [1,2,4,3,5,67,5,4,7,8,9];
- +var min =0
- +var max =100
- +for (x in array):
- +	if(x < min):
- +		x=min;
- +	if(x > max):
- +		x=max;
- +
- +document.log(min , max);
+var min =10000
+var max =0
+for (x in array){
+	
+	if(array[x] < min)
+	{	console.log(min);
+		min=array[x];
+	}
+	if(array[x] > max)
+	{
+		console.log(max);
+		max=array[x];
+	}
+}
+
+console.log("min : " + min + " max : " + max)
